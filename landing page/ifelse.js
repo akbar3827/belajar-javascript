83296da9e1f6fd0cdb2ec1d943c380d606c6b886
fauzan2720 <fauzan.abdillah2705@gmail.com>
2025-07-21 18:02:12 +0700

    belajar-javascript

---
+++ landing page/ifelse.js
@@ -0,0 +1,10 @@
+let jummlahAngkot = 10;
+    angkotBeroperasi = 6;
+    noAngkot = 1;
+
+if (noAngkot <= angkotBeroperasi) {
+    console.log ('angkot No.' + noAngkot + ' beroperasi dengan baik');
+} if (noAngkot >= angkotBeroperasi) {
+    console.log ('angkot No.' + noAngkot + ' sedang tidak beroperasi');
+    noAngkot++
+}
